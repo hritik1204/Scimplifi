791cac23827977df34eca0e26405e1ede8378368
Hritik Sharma <hritik1204@gmail.com>
2023-06-07 23:44:01 +0530

    firebase config changes

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKPWLT-qODFrqQFWCuGYwCm88mFHEYOs8",
-  authDomain: "dashboard-388712.firebaseapp.com",
-  projectId: "dashboard-388712",
-  storageBucket: "dashboard-388712.appspot.com",
-  messagingSenderId: "765164466331",
-  appId: "1:765164466331:web:a33ea0d4f01e8b61aa963e",
-  measurementId: "G-M4FV14J30F",
+  apiKey: "AIzaSyDUnY-MF7jhOoEG8dWH2wemYkNWcL2dklI",
+  authDomain: "openinappdashboard.firebaseapp.com",
+  projectId: "openinappdashboard",
+  storageBucket: "openinappdashboard.appspot.com",
+  messagingSenderId: "966136944590",
+  appId: "1:966136944590:web:fc419131d9092b122e7d9f",
+  measurementId: "G-X77294XXCN",
 };
 
 // Initialize Firebase
